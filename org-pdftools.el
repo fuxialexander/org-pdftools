0c4189020431281c4359a4dac22410c9ddaedacf
Alexander Fu Xi <fuxialexander@users.noreply.github.com>
2020-04-20 15:52:18 +0800

    fix isearch quote
    fix https://github.com/fuxialexander/org-pdftools/pull/30

--- org-pdftools.el
+++ org-pdftools.el
@@ -324,7 +324,7 @@ Can be one of highlight/underline/strikeout/squiggly."
                                                     (mapconcat 'identity (pdf-view-active-region-text) ? ))))
                 (page (number-to-string (pdf-view-current-page)))
                 (link (org-pdftools-get-link))
-                (isearchstr (if (string-match ".*??\\(.*\\)" link)
+                (isearchstr (if (string-match (concat ".*" (regexp-quote org-pdftools-search-string-separator) "\\(.*\\)") link)
                                 (match-string 1 link)))
                 (desc (funcall org-pdftools-get-desc-function file page (or quot isearchstr))))
            (org-link-store-props
